f41240be911e2d2269aa60d1a04cbb5d875cf76a
midhushsai <midhushsai84@gmail.com>
2026-05-05 22:26:08 +0530

    Update meteor_detection.py

--- Q1_meteor_detection/meteor_detection.py
+++ Q1_meteor_detection/meteor_detection.py
@@ -1,1 +1,21 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+from scipy.signal import find_peaks
 
+# Load data
+data = pd.read_csv("data/meteor_data.csv")
+
+# Extract signal column
+signal = data['signal']
+
+# Plot signal
+plt.plot(signal)
+plt.title("Signal Data")
+plt.show()
+
+# Detect peaks (meteor events)
+peaks, _ = find_peaks(signal, height=threshold)
+
+# Print result
+print("Number of meteors detected:", len(peaks))
